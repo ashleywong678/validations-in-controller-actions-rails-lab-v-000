ab481681f0fe97124998394f0cbd52b62ea24185
Ashley Wong <ashwong04@gmail.com>
2019-04-04 21:22:23 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,8 +8,8 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post = Post.new(post_params)
-    if @post.valid?
+    post = Post.new(post_params)
+    if post.valid?
       @post.update(post_params)
       redirect_to post_path(@post)
     else
